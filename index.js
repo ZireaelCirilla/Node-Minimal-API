219b346fd04d321a6fed83018e0633eded2618f9
Marce Graffigna <marcesmartphone@gmail.com>
2019-08-09 20:08:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import all your needs
+const express = require('express')
+const app = express()
+
+var firebase = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://twittahah.firebaseio.com"
+});
+
+const usersRouter = require('./src/users')
+
+//Use all your middlewares
+app.use(express.json())
+app.use('/api/users', usersRouter)
+app.use('/api/tweets', tweetsRouter)
+
+const PORT = 8080
+const HOST = '0.0.0.0'
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`)
---
+++ src/users/users.controller.js
@@ -0,0 +1,62 @@
+//Imports
+
+/**
+ * POST     /api/users      -> create
+ * GET      /api/users      -> getAll
+ * GET      /api/users/:id  -> getById
+ * PUT      /api/users/:id  -> update
+ * DELETE   /api/users/:id  -> remove
+ */
+
+module.exports = {
+    create,
+    getAll,
+    getById,
+    update,
+    remove
+}
+
+/**
+ * Creates a new ...
+ * @param {request} req Request
+ * @param {*} res Response
+ */
+function create(req, res) {
+
+}
+
+/**
+ * Returns all ...
+ * @param {request} req Request
+ * @param {*} res Response
+ */
+function getAll(req, res) {
+
+}
+
+/**
+ * Returns one ... by Id
+ * @param {request} req Request
+ * @param {*} res Response
+ */
+function getById(req, res) {
+
+}
+
+/**
+ * Updates one ... by Id
+ * @param {request} req Request
+ * @param {*} res Response
+ */
+function update(req, res) {
+
+}
+
+/**
+ * Removes one ... by Id
+ * @param {request} req Request
+ * @param {*} res Response
+ */
+function remove(req, res) {
+
+}
